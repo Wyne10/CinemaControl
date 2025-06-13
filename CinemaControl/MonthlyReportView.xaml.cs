f39de5291747c0c13412ba799338ab7ad1e42538
Wyne10 <izmodenov1997@gmail.com>
2025-06-13 23:33:27 +0500

    Add Monthly Report Tab and Refactor Weekly Report Layout

---
+++ CinemaControl/MonthlyReportView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CinemaControl
+{
+    public partial class MonthlyReportView : UserControl
+    {
+        public MonthlyReportView()
+        {
+            InitializeComponent();
+        }
+    }
+} 
