30d33d83857d2c42fc821182ce5619372dcf4368
Wyne10 <izmodenov1997@gmail.com>
2025-06-13 23:14:22 +0500

    Refactor application startup and remove MainWindow. Updated StartupUri to MainView.xaml and deleted MainWindow.xaml and its code-behind file. Added CETCompat property to project file.

---
+++ CinemaControl/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace CinemaControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CinemaControl/WeeklyReportView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CinemaControl
+{
+    public partial class WeeklyReportView : UserControl
+    {
+        public WeeklyReportView()
+        {
+            InitializeComponent();
+        }
+    }
+} 
