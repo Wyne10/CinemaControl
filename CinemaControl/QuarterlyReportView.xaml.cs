f2cfff21a175a1fbbb8654e1ee5e5961ed43a17e
Wyne10 <izmodenov1997@gmail.com>
2025-06-13 23:43:23 +0500

    Add Quarterly Report Tab and Update Weekly Report Button and Label Text

---
+++ CinemaControl/QuarterlyReportView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CinemaControl
+{
+    public partial class QuarterlyReportView : UserControl
+    {
+        public QuarterlyReportView()
+        {
+            InitializeComponent();
+        }
+    }
+} 
