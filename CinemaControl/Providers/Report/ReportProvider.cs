133d3459fee064452e842be311789d11271b0337
Wyne10 <izmodenov1997@gmail.com>
2025-06-18 11:59:46 +0500

    Bug fix

--- CinemaControl/Providers/Report/ReportProvider.cs
+++ CinemaControl/Providers/Report/ReportProvider.cs
@@ -6,13 +6,13 @@ public class ReportProvider : IReportProvider
 {
     public virtual async Task<IDownload> DownloadReport(IPage page, IFrame frame, ReportSaveType saveType)
     {
-        await frame.ClickAsync(IReportProvider.ViewReportButtonSelector);
+        await frame.Locator(IReportProvider.ViewReportButtonSelector).ClickAsync();
         await frame.WaitForLoadStateAsync(LoadState.NetworkIdle);
-        await frame.ClickAsync(IReportProvider.ExportMenuLinkSelector);
-        await frame.WaitForSelectorAsync(saveType.Selector);
+        await frame.Locator(IReportProvider.ExportMenuLinkSelector).ClickAsync();
+        await frame.Locator(saveType.Selector).WaitForAsync();
             
         var downloadTask = page.WaitForDownloadAsync();
-        await frame.ClickAsync(saveType.Selector);
+        await frame.Locator(saveType.Selector).ClickAsync();
 
         return await downloadTask;
     }
