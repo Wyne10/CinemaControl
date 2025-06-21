4b93e0599c9e9546c85904f3682b1d1221e6f5b5
Wyne10 <izmodenov1997@gmail.com>
2025-06-21 15:20:21 +0500

    Fix composite report service

--- CinemaControl/Services/CompositeReportService.cs
+++ CinemaControl/Services/CompositeReportService.cs
@@ -2,11 +2,19 @@
 
 namespace CinemaControl.Services;
 
-public class CompositeReportService(IEnumerable<IReportService> reportServices) : ReportService
+public class CompositeReportService : ReportService
 {
+    private readonly IEnumerable<IReportService> _reportServices;
+    
+    public CompositeReportService(IEnumerable<IReportService> reportServices)
+    {
+        _reportServices = reportServices;
+        foreach (IReportService reportService in _reportServices) reportService.OnDownloadProgress += ProgressDownload;
+    }
+    
     public override async Task<string> GenerateReportFiles(DateTime from, DateTime to, IPage page)
     {
-        foreach(IReportService reportService in reportServices) await reportService.GenerateReportFiles(from, to, page);
+        foreach(IReportService reportService in _reportServices) await reportService.GenerateReportFiles(from, to, page);
         return GetSessionPath(from, to);
     }
 }
