daeb0ecd2dfb9434e5c229b178059c628e77b19b
Wyne10 <izmodenov1997@gmail.com>
2025-06-21 19:59:13 +0500

    Bug fix

--- CinemaControl/App.xaml.cs
+++ CinemaControl/App.xaml.cs
@@ -30,8 +30,6 @@ public partial class App
             {
                 services.Configure<AppConfiguration>(
                     context.Configuration.GetRequiredSection("App"));
-                services.Configure<WeeklyReportConfiguration>(
-                    context.Configuration.GetRequiredSection("WeeklyReport"));
                 services.Configure<MonthlyReportConfiguration>(
                     context.Configuration.GetRequiredSection("MonthlyReport"));
                 services.Configure<QuarterlyReportConfiguration>(
