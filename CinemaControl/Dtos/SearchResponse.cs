cf02172b51aadd8331ff05316d9828ade62124d3
Wyne10 <izmodenov1997@gmail.com>
2025-06-21 17:02:44 +0500

    Remove dtos setters

--- CinemaControl/Dtos/SearchResponse.cs
+++ CinemaControl/Dtos/SearchResponse.cs
@@ -1,115 +1,116 @@
 using System.Text.Json.Serialization;
+// ReSharper disable All
 
 namespace CinemaControl.Dtos;
 
-public class SearchResponse
+public record SearchResponse
 {
     [JsonPropertyName("docs")]
-    public List<Movie>? Docs { get; set; }
+    public List<Movie>? Docs { get; init; }
 
     [JsonPropertyName("total")]
-    public int Total { get; set; }
+    public int Total { get; init; }
 
     [JsonPropertyName("limit")]
-    public int Limit { get; set; }
+    public int Limit { get; init; }
 
     [JsonPropertyName("page")]
-    public int Page { get; set; }
+    public int Page { get; init; }
 
     [JsonPropertyName("pages")]
-    public int Pages { get; set; }
+    public int Pages { get; init; }
 }
 
-public class Movie
+public record Movie
 {
     [JsonPropertyName("id")]
-    public int Id { get; set; }
+    public int Id { get; init; }
 
     [JsonPropertyName("name")]
-    public string? Name { get; set; }
+    public string? Name { get; init; }
 
     [JsonPropertyName("alternativeName")]
-    public string? AlternativeName { get; set; }
+    public string? AlternativeName { get; init; }
         
     [JsonPropertyName("year")]
-    public int? Year { get; set; }
+    public int? Year { get; init; }
 
     [JsonPropertyName("description")]
-    public string? Description { get; set; }
+    public string? Description { get; init; }
 
     [JsonPropertyName("shortDescription")]
-    public string? ShortDescription { get; set; }
+    public string? ShortDescription { get; init; }
     
     [JsonPropertyName("movieLength")]
-    public int? Length { get; set; } 
+    public int? Length { get; init; } 
     
     [JsonPropertyName("ageRating")]
-    public int? AgeRating { get; set; } 
+    public int? AgeRating { get; init; } 
         
     [JsonPropertyName("logo")]
-    public Logo? Logo { get; set; }
+    public Logo? Logo { get; init; }
     
     [JsonPropertyName("poster")]
-    public Poster? Poster { get; set; }
+    public Poster? Poster { get; init; }
 
     [JsonPropertyName("rating")]
-    public Rating? Rating { get; set; }
+    public Rating? Rating { get; init; }
 
     [JsonPropertyName("genres")]
-    public List<Genre>? Genres { get; set; }
+    public List<Genre>? Genres { get; init; }
 
     [JsonPropertyName("countries")]
-    public List<Country>? Countries { get; set; }
+    public List<Country>? Countries { get; init; }
     
     public bool IsRussian() => Countries?.Any(c => c.Name == "Россия") ?? false;
 
     public bool IsChildrenAvailable() => AgeRating <= 6;
 }
 
-public class Logo
+public record Logo
 {
     [JsonPropertyName("url")]
-    public string? Url { get; set; }
+    public string? Url { get; init; }
 }
 
-public class Poster
+public record Poster
 {
     [JsonPropertyName("url")]
-    public string? Url { get; set; }
+    public string? Url { get; init; }
 
     [JsonPropertyName("previewUrl")]
-    public string? PreviewUrl { get; set; }
+    public string? PreviewUrl { get; init; }
 }
 
-public class Rating
+public record Rating
 {
     [JsonPropertyName("kp")]
-    public double? Kp { get; set; }
+    public double? Kp { get; init; }
         
     [JsonPropertyName("imdb")]
-    public double? Imdb { get; set; }
+    public double? Imdb { get; init; }
     
     [JsonPropertyName("tmdb")]
-    public double? Tmdb { get; set; }
+    public double? Tmdb { get; init; }
     
     [JsonPropertyName("filmCritics")]
-    public double? FilmCritics { get; set; }
+    public double? FilmCritics { get; init; }
     
     [JsonPropertyName("russianFilmCritics")]
-    public double? RussianFilmCritics { get; set; }
+    public double? RussianFilmCritics { get; init; }
     
     [JsonPropertyName("await")]
-    public double? Await { get; set; }
+    public double? Await { get; init; }
 }
     
-public class Genre
+public record Genre
 {
     [JsonPropertyName("name")]
-    public string? Name { get; set; }
+    public string? Name { get; init; }
 }
 
-public class Country
+public record Country
 {
     [JsonPropertyName("name")]
-    public string? Name { get; set; }
+    public string? Name { get; init; }
 }
