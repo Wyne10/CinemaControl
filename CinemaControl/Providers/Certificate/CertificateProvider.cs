eaaa89b6149b37d0bbaa1ca9028c7c9907ab8d48
Wyne10 <izmodenov1997@gmail.com>
2025-07-04 21:15:09 +0500

    Fix incorrect table selector Skip first row in certificate table Trim table data

--- CinemaControl/Providers/Certificate/CertificateProvider.cs
+++ CinemaControl/Providers/Certificate/CertificateProvider.cs
@@ -9,7 +9,7 @@ public class CertificateProvider : ICertificateProvider
         var certificates = new Dictionary<string, string>();
         
         await page.GotoAsync(ICertificateProvider.MovieArchiveUrl);
-        var tableLocator = page.Locator(ICertificateProvider.TableSelector).Nth(1);
+        var tableLocator = page.Locator(ICertificateProvider.TableSelector).Nth(2);
         
         foreach (var certificate in await ParseTable(tableLocator))
             certificates[certificate.Key] = certificate.Value;
@@ -33,13 +33,13 @@ public class CertificateProvider : ICertificateProvider
         var certificates = new Dictionary<string, string>();
         var rows = await table.Locator("tr").AllAsync();
 
-        foreach (var row in rows)
+        foreach (var row in rows.Skip(1))
         {
             var cells = await row.Locator("td").AllAsync();
 
             var movieName = await cells[ICertificateProvider.MovieNameColumnIndex].InnerTextAsync();
             var certificate = await cells[ICertificateProvider.CertificateColumnIndex].InnerTextAsync();
-            certificates[movieName] = certificate;
+            certificates[movieName.Trim()] = certificate.Trim();
         }
 
         return certificates;
